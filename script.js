eb3999fe859d12c60c339429791cac34c7caa92e
Bui Viet <buiviet57.v5@gmail.com>
2023-10-14 22:55:46 +0700

    slider & footer

---
+++ script.js
@@ -0,0 +1,19 @@
+//slider
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+  });
+
+// show search
+const searchButton = document.querySelector('.t-search'),
+    tClose = document.querySetector('.search-close'),
+    showClass = document.querySelector('.site');
+searchButton.addEventListener('click', function() {
+    showClass.classList.toggle('showsearch')
+})
+tClose.addEventListener('click', function(){
+    showClass.classList.remove('showsearch')
+})
