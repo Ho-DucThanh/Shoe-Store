959ced99edc39ffc95b9f5804bbd23b5e08d3fbd
LQThai1603 <lqt1603@gmail.com>
2023-10-16 23:16:52 +0700

    done header

--- script.js
+++ script.js
@@ -31,6 +31,13 @@ closeButton.addEventListener('click',function(){
     addclass.classList.remove('showmenu');
 })
 
+//slider
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+})
 
 
 // show sub menu on mobile
@@ -43,4 +50,3 @@ function toggle(e){
     if(this.closest('.has-child').classList != 'expand');
     this.closest('.has-child').classList.toggle('expand')
 }
-
